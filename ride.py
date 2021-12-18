6eb38270d70c69155c38392e689e19cb404ae098
lightspd547 <lsun0547@gmail.com>
2021-12-17 21:25:03 -0500

    init

---
+++ ride.py
@@ -0,0 +1,29 @@
+"""
+ID: lsun0541
+LANG: PYTHON2
+TASK: ride
+"""
+
+fin = open ('ride.in', 'r')
+fout = open ('ride.out', 'w')
+
+g = fin.readline()
+u = fin.readline()
+
+value_g = 1
+value_u = 1
+for x in g:
+    v = (ord(x) - 64)
+    if v > 0:
+        value_g *= v
+for x in u:
+    v = (ord(x) - 64)
+    if v > 0:
+        value_u *= v
+if (value_g % 47 == value_u % 47):
+    fout.write("GO\n")
+else:
+    fout.write("STAY\n")
+
+fin.close()
+fout.close()
